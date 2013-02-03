65c592cc55cc8d336c80036a1273e9a32697e061
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-02-03 17:04:06 +0900

    create a project

---
+++ SpeakingClock/src/net/kaoriya/speakingclock/TestActivity.java
@@ -0,0 +1,14 @@
+package net.kaoriya.speakingclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+    }
+}
