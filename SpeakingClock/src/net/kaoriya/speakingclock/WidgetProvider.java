2e76e5d6bfb8ac7bda13fb7dcc576e9e84ac3cb5
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-02-03 22:01:54 +0900

    add switching widget.

---
+++ SpeakingClock/src/net/kaoriya/speakingclock/WidgetProvider.java
@@ -0,0 +1,34 @@
+package net.kaoriya.speakingclock;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public final class WidgetProvider extends AppWidgetProvider
+{
+    public static final String TAG = "SCWidget";
+
+    // @implements AppWidgetProvider
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        context.startService(new Intent(context, SpeakingClockService.class));
+    }
+
+    // @implements AppWidgetProvider
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        context.stopService(new Intent(context, SpeakingClockService.class));
+    }
+
+    // @implements AppWidgetProvider
+    public void onUpdate(
+            Context context,
+            AppWidgetManager manager,
+            int[] ids)
+    {
+        super.onUpdate(context, manager, ids);
+        // TODO:
+    }
+
+}
